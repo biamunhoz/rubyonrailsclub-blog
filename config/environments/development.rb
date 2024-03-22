2af8f2fa2d3bca5a6ab3ca067a62ebdf24f98a17
Fabiana Rodrigues <biamunhoz@gmail.com>
2024-03-22 14:43:42 -0300

    adicionando autenticacao com devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,10 +75,12 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  # config.generators.after_generate do |files|
-  #   parsable_files = files.filter { |file| file.end_with(".rb") }
-  #   unless parsable_files.empty?
-  #     system("bundle exec rubocop -A --fail-level=E #{files.shelljoin}", exception: true)
-  #   end
-  # end
+  config.generators.after_generate do |files|
+    parsable_files = files.filter { |file| file.end_with?(".rb") }
+    unless parsable_files.empty?
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
+  end
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema[7.1].define(version: 202403071709202) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
+  create_table "admins", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "articles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
     t.text "body"
