9cab9f44d43d0780056ce660bc7cf2e151adfb8d
Fabiana Rodrigues <biamunhoz@gmail.com>
2024-08-21 16:04:31 -0300

    Adicionando o nome do usuário no Devise

--- app/controllers/administrate/categories_controller.rb
+++ app/controllers/administrate/categories_controller.rb
@@ -1,99 +1,100 @@
 # frozen_string_literal: true
 
 module Administrate
-    class CategoriesController < AdministrateController
-      before_action :set_category, only: [:show, :edit, :update, :destroy]
-      # before_action :set_categories, only: [:new, :edit, :show]
-  
-      # GET /category or /category.json
-      def index
-        @categories = Category.all
-      end
-  
-    #   # GET /category/1 or /category/1.json
-      def show
-      end
-  
-    #   # GET /category/new
-      def new
-        @category = Category.new
-      end
-  
-    #   # GET /category/1/edit
-      def edit
-      end
-  
-    #   # POST /category or /category.json
-      def create
-        @category = Category.new(category_params)
-  
-        respond_to do |format|
-          if @category.save
-            format.html { redirect_to(administrate_category_url(@category), notice: "Categoria foi criado com sucesso.") }
-            format.json { render(:show, status: :created, location: @category) }
-          else
-            format.html { render(:new, status: :unprocessable_entity) }
-            format.json { render(json: @category.errors, status: :unprocessable_entity) }
-          end
-        end
-      end
-  
-    #   # PATCH/PUT /category/1 or /category/1.json
-      def update
-        respond_to do |format|
-          if @category.update(category_params)
-            format.html { redirect_to(administrate_category_url(@category), notice: "Categoria foi atualizada com sucesso") }
-            format.json { render(:show, status: :ok, location: @category) }
-          else
-            format.html { render(:edit, status: :unprocessable_entity) }
-            format.json { render(json: @category.errors, status: :unprocessable_entity) }
-          end
-        end
-      end
-  
-    #   # DELETE /category/1 or /category/1.json
-      def destroy
-        
-  
-        respond_to do |format|
-          format.html do 
-            if @category.articles.count > 0
-              redirect_to(
-                administrate_categories_url, 
-                alert: "Existem artigos associados a essa categoria. Não é possível apagá-la."
-                )   
-            else 
-              @category.destroy!
-              redirect_to(administrate_categories_url, notice: "Categoria foi apagada com sucesso") 
-            end 
+  class CategoriesController < AdministrateController
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
+    # before_action :set_categories, only: [:new, :edit, :show]
 
-          end           
-          format.json { head(:no_content) }
+    # GET /category or /category.json
+    def index
+      @categories = Category.all
+    end
+
+    #   # GET /category/1 or /category/1.json
+    def show
+    end
+
+    #   # GET /category/new
+    def new
+      @category = Category.new
+    end
+
+    #   # GET /category/1/edit
+    def edit
+    end
+
+    #   # POST /category or /category.json
+    def create
+      @category = Category.new(category_params)
+
+      respond_to do |format|
+        if @category.save
+          format.html do
+            redirect_to(administrate_category_url(@category), notice: "Categoria foi criado com sucesso.")
+          end
+          format.json { render(:show, status: :created, location: @category) }
+        else
+          format.html { render(:new, status: :unprocessable_entity) }
+          format.json { render(json: @category.errors, status: :unprocessable_entity) }
         end
       end
-  
+    end
+
+    #   # PATCH/PUT /category/1 or /category/1.json
+    def update
+      respond_to do |format|
+        if @category.update(category_params)
+          format.html do
+            redirect_to(administrate_category_url(@category), notice: "Categoria foi atualizada com sucesso")
+          end
+          format.json { render(:show, status: :ok, location: @category) }
+        else
+          format.html { render(:edit, status: :unprocessable_entity) }
+          format.json { render(json: @category.errors, status: :unprocessable_entity) }
+        end
+      end
+    end
+
+    #   # DELETE /category/1 or /category/1.json
+    def destroy
+      respond_to do |format|
+        format.html do
+          if @category.articles.count > 0
+            redirect_to(
+              administrate_categories_url,
+              alert: "Existem artigos associados a essa categoria. Não é possível apagá-la.",
+            )
+          else
+            @category.destroy!
+            redirect_to(administrate_categories_url, notice: "Categoria foi apagada com sucesso")
+          end
+        end
+        format.json { head(:no_content) }
+      end
+    end
+
     #   def destroy_cover_image
     #     category.cover_image.purge
-  
+
     #     respond_to do |format|
     #       format.turbo_stream { render(turbo_stream: turbo_stream.remove(category)) }
     #     end
     #   end
-  
+
     #   private
-  
+
     #   # Use callbacks to share common setup or constraints between actions.
-      def set_category
-        @category = Category.find(params[:id])
-      end
-  
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
     #   def set_categories
     #     @categories = Category.all
     #   end
-  
+
     #   # Only allow a list of trusted parameters through.
-      def category_params
-        params.require(:category).permit(:name)
-      end
+    def category_params
+      params.require(:category).permit(:name)
     end
-end  
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,17 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-admins = ["admin@admin.com", "test@test.com"]
+admins = {
+  "admin@admin.com" => "Administrador",
+  "test@test.com" => "Teste",
+}
 
-admins.each do |email|
+admins.each do |email, name|
   next if Admin.find_by(email: email)
 
   Admin.create!(
     email:,
+    name:,
     password: ENV["DEFAULT_PASSWORD"],
     password_confirmation: ENV["DEFAULT_PASSWORD"],
   )
