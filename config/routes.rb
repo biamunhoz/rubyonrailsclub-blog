3139093d4193c6a0024509d68d9057c53b40a142
Fabiana Rodrigues <biamunhoz@gmail.com>
2024-09-05 15:10:49 -0300

    Adicionando o CRUD do autor na área administrativa

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     end
 
     resources :categories
+    resources :authors
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
