a2b253d3dd8fb595fda8d0050dcb3842d39a7233
Fabiana Rodrigues <biamunhoz@gmail.com>
2024-09-10 11:14:04 -0300

    Adicionando o autor na edição de artigos da área administrativa

--- app/controllers/administrate/articles_controller.rb
+++ app/controllers/administrate/articles_controller.rb
@@ -4,6 +4,7 @@ module Administrate
   class ArticlesController < AdministrateController
     before_action :set_article, only: [:show, :edit, :update, :destroy, :destroy_cover_image]
     before_action :set_categories, only: [:new, :edit, :show]
+    before_action :set_authors, only: [:new, :edit, :show]
 
     # GET /articles or /articles.json
     def index
@@ -82,9 +83,13 @@ module Administrate
       @categories = Category.all
     end
 
+    def set_authors
+      @authors = Author.all
+    end
+
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :body, :category_id, :cover_image)
+      params.require(:article).permit(:title, :body, :category_id, :cover_image, :author_id)
     end
   end
 end
